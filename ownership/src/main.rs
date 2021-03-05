8e90dcaafca9d83a5f18289c56c25cb6ea0cd97e
huangzhongjun@sisyphe.com.cn <huangzhongjun@sisyphe.com.cn>
2021-03-05 15:49:53 +0800

    加一点点

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -11,6 +11,24 @@ fn main() {
 
     let mut s = String::from("what the");
     mut_reference(&mut s);
+
+    let (s, len) = multiple_return_value(s);
+    println!("s:{};len:{}", s, len);
+    
+    println!("---------------------寂寞分割线---------------------");
+
+    let len = calculate_length(&s);
+    println!("s:{};len:{}", s, len);
+    
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn multiple_return_value(s: String) -> (String, usize) {
+    let len = s.len();
+    (s, len)
 }
 
 fn mut_reference(s: &mut String) {
