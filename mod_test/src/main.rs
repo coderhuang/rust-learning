90a05c73e4471aa4f75f35fb62953d398bfb4574
huangzhongjun@sisyphe.com.cn <huangzhongjun@sisyphe.com.cn>
2021-03-08 17:21:18 +0800

    添加用例

--- mod_test/src/main.rs
+++ mod_test/src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     println!("Hello, mod_test!");
 
     my_hosting::inner::call_print_one();
+    my_hosting::call_print_two();
 }
 
 
@@ -12,9 +13,17 @@ mod my_hosting{
         pub fn call_print_one(){
             super::print_one();
         }
+
+        pub fn print_two(){
+            println!("two");
+        }
     }
 
     fn print_one(){
         println!("one");
+    }
+
+    pub fn call_print_two(){
+        self::inner::print_two();
     }
 }
