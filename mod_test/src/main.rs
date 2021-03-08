6f8809213ff12beeb5596c9c984b6c30fc65ab60
huangzhongjun@sisyphe.com.cn <huangzhongjun@sisyphe.com.cn>
2021-03-08 18:04:44 +0800

    添加用例

--- mod_test/src/main.rs
+++ mod_test/src/main.rs
@@ -3,27 +3,56 @@ fn main() {
 
     my_hosting::inner::call_print_one();
     my_hosting::call_print_two();
+
+    let salad_breakfast = my_hosting::Breakfast::of(String::from("salad"));
+    println!("salad_breakfast:{:#?}", salad_breakfast);
+
+    let spring = my_hosting::Season::Spring;
+    if let my_hosting::Season::Spring = spring {
+        println!("spring is coming");
+    }
 }
 
+mod my_hosting {
 
-
-mod my_hosting{
-
-    pub mod inner{
-        pub fn call_print_one(){
+    pub mod inner {
+        pub fn call_print_one() {
             super::print_one();
         }
 
-        pub fn print_two(){
+        pub fn print_two() {
             println!("two");
         }
     }
 
-    fn print_one(){
+    fn print_one() {
         println!("one");
     }
 
-    pub fn call_print_two(){
+    pub fn call_print_two() {
         self::inner::print_two();
+    }
+
+    #[derive(Debug)]
+    pub struct Breakfast {
+        pub totast: String,
+        season_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn of(totast: String) -> Breakfast {
+            Breakfast {
+                totast: totast,
+                season_fruit: String::from("apple"),
+            }
+        }
+    }
+
+    #[derive(Debug)]
+    pub enum Season {
+        Spring,
+        Summer,
+        Autumn,
+        Winter,
     }
 }
